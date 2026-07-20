a68287c5e55b4240b235225001afa44f4c14b2c7
ABDULHAFEEZ ABDULGANNY <abdulgannyabdulhafeez0@gmail.com>
2026-07-20 17:12:10 +0100

    Update index.js

--- apps/api-gateway/index.js
+++ apps/api-gateway/index.js
@@ -3,8 +3,15 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 
+// Route to User Service
 app.use('/users', createProxyMiddleware({ 
     target: 'http://localhost:3001', 
+    changeOrigin: true 
+}));
+
+// Route to Cart Service
+app.use('/cart', createProxyMiddleware({ 
+    target: 'http://localhost:3002', 
     changeOrigin: true 
 }));
 
